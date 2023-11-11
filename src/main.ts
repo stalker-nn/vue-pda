07ec950772e0492a79adac7c4b7e070c27e0b0a8
Zimin Alex <zimin@npods.ru>
2023-11-11 23:30:45 +0300

    Add mdi

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,16 @@ import router from './router'
 
 //Vuetify
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from "vuetify";
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 const vuetify = createVuetify({
     components,
     directives,
-})
+});
 
 const app = createApp(App)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Dashboard from '../components/Dashboard/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'dashboard',
+      component: Dashboard
     },
   ]
 })
